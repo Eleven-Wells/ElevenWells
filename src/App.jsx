1c7e20fdac4d85675b4f148673fcee5804e57064
Tøbé Çhükwû <astrolix278@gmail.com>
2025-11-19 12:49:29 +0100

    feat(footer): added the footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navigation from './components/ui/Navigation.jsx'
+import Footer from './components/ui/Footer.jsx'
 
 const App = () => {
   return (
     <div>
       <Navigation />
+      <Footer />
     </div>
   )
 }
